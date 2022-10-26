3b8f9e1568bf806181933dfe85521f5f6f3e156f
Asko <asko.hulkko@voco.ee>
2022-10-26 14:58:29 +0300

    Ülesanne: Funktsiooni mõiste

--- functions.py
+++ functions.py
@@ -17,21 +17,30 @@ print(get_hello())
 def ask_name_and_greet_user():
     name = input("What's your name? : ")
     name = name.capitalize()
-    print(f"Hi. {name} Would you like to have a Hamburger?")
+    if name == "Thanos"
+        print("Get out of here, Thanos! Nobody wants to play with you!")
+    else:
+        print(f"Hi. {name} Would you like to have a Hamburger?")
     pass
 
 ask_name_and_greet_user()
 
 
 def calculate_hypotenuse_length(cathetus_a, cathetus_b):
-    hypotenuse = math.sqrt((cathetus_a ** 2) + (cathetus_b ** 2))
+    if cathetus_a and cathetus_b is int:
+        hypotenuse = math.sqrt((cathetus_a ** 2) + (cathetus_b ** 2))
+    else:
+        print("Use full numbers not numbers with decimals in the numbers!")
     return hypotenuse
 
 print(calculate_hypotenuse_length(3, 4))
 
 
 def calculate_cathetus_length(cathetus_a, hypotenuse):
-    cathetus_b = math.sqrt((hypotenuse ** 2) - (cathetus_a ** 2))
+    if cathetus_a and hypotenuse is int:
+        cathetus_b = math.sqrt((hypotenuse ** 2) - (cathetus_a ** 2))
+    else:
+        print("Use full numbers not numbers with decimals in the numbers!")
     return cathetus_b
 
 print(calculate_cathetus_length(3, 5))
