a18b9771f5805372827d8d783c056e886cc1ff5a
Asko <asko.hulkko@voco.ee>
2022-10-26 14:44:57 +0300

    Ülesanne: Kasutaja sisend ja arvutused

---
+++ user input and calculations.py
@@ -0,0 +1,11 @@
+name = input("Sisestage oma nimi: ")
+name = name.capitalize()
+allowed_speed = input("Sisestage lubatud kiirus (km/h): ")
+actual_speed = input("Sisestage tegelik kiirus (km/h): ")
+
+if allowed_speed < actual_speed:
+    fine =(int(actual_speed) - int(allowed_speed)) * 3
+    fine = min(192, fine)
+    print(f"{name}, kiiruse ületamise eest on teie trahv {fine} eurot.")
+else:
+    print(f"{name}, te ei ole ületanud kiirust.")
