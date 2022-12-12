2c53bea4fb74ab3650484dd1a39b70d2bb2adeb1
Asko <asko.hulkko@voco.ee>
2022-12-12 14:48:42 +0200

    Moving files

---
+++ Mata ja loogika/trivia.py
@@ -0,0 +1,16 @@
+# Trivia
+
+# 4 questions
+# with score
+# every score multiplies by a 100
+
+score = 1
+ans = input("Is the sky blue? [1 for yes/ 0 for no] ")
+if int(ans) == 1:
+    score *= 100
+    print(f"correct\nyour score is now {score}.")
+else:
+    print("yo dumb as fuck.")
+    print("Your score is still 0. RIP + L + Bozo")
+
+print("now onto the next question!")
